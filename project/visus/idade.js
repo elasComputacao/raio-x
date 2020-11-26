8ce5a80b7883fece59324b41f857c7fe15ea4b38
Beatriz Miranda <beatriza.dmiranda@gmail.com>
2020-11-26 17:49:23 -0300

    editando link do banco de ideias, mudando cor idade e adicionando legenda na situação academica

--- project/visus/idade.js
+++ project/visus/idade.js
@@ -1,35 +1,20 @@
 var yourV5Spec = {
-    "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
-    "data": {"url": "https://raw.githubusercontent.com/elasComputacao/raio-x-dados/main/data/dados-processados/mulheres.csv"},
-    "mark": {"type":"bar", "tooltip": true},
-    "title": {
-      "text":"Idade de ingresso das ingressantes, de 2000.1 até 2020.1",
-      "fontSize": 15
-    },
-    "transform": [
-      {
-        "filter": "datum.idade_ingresso > 0",
-        "filter": "datum.periodo_ingresso >= 2000.1"
-      }
-    ],
-    "width": 700,
-    "height": 200,
-    "encoding": {
-      "x": {
-        "field": "idade_ingresso",
-        "title": "Idade de ingresso"
-      },
-      "y": {
-        "aggregate": "count",
-        "title": "Quantidade"
-      },
-      "color": {
-         "scale": {
-          "range": ["#F1C40F"]
-        }
-      },
-    }
+  "$schema": "https://vega.github.io/schema/vega-lite/v4.json",
+  "data": {
+    "url": "https://raw.githubusercontent.com/elasComputacao/raio-x-dados/main/data/dados-processados/mulheres.csv"
+  },
+  "mark": {"type": "bar",  "color": "#F1C40F","tooltip": true},
+  "title": {
+    "text": "Idade de ingresso das ingressantes, de 2000.1 até 2020.1",
+    "fontSize": 15
+  },
+  "transform": [{"filter": "datum.periodo_ingresso >= 2000.1"}],
+  "width": 700,
+  "height": 200,
+  "encoding": {
+    "x": {"field": "idade_ingresso", "title": "Idade de ingresso"},
+    "y": {"aggregate": "count", "title": "Quantidade"}
   }
-  ;
-  vegaEmbed("#vis5", yourV5Spec);
+};
+vegaEmbed("#vis5", yourV5Spec);
   
